2332751633cef819282def160a077095af6e74e6
triton <triton.68@mail.ru>
2020-03-26 22:24:35 +0300

    init: set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:__dirname + "/src/index.html",
+      inject: 'body'
+    })
+  ]
+
+
+};
