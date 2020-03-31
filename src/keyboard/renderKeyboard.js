28c1a9d929b1574e245443bb648b19f69bba1370
triton <triton.68@mail.ru>
2020-03-31 04:32:40 +0300

    refactor renderKeyboard; delete editorconfig

--- src/keyboard/renderKeyboard.js
+++ src/keyboard/renderKeyboard.js
@@ -5,28 +5,24 @@ import {
   renderKeyEnShift,
 } from './key/key';
 
-// какую клаву будем рисовать, массив с клавишами, куда
 function renderKeyboard(param, arr, root) {
+  document.getElementById(root).innerHTML = '';
   if (param === 'renderKeyRu') {
-    document.getElementById(root).innerHTML = '';
     arr.forEach((el) => {
       renderKeyRu(el, root);
     });
   }
   if (param === 'renderKeyEn') {
-    document.getElementById(root).innerHTML = '';
     arr.forEach((el) => {
       renderKeyEn(el, root);
     });
   }
   if (param === 'renderKeyRuShift') {
-    document.getElementById(root).innerHTML = '';
     arr.forEach((el) => {
       renderKeyRuShift(el, root);
     });
   }
   if (param === 'renderKeyEnShift') {
-    document.getElementById(root).innerHTML = '';
     arr.forEach((el) => {
       renderKeyEnShift(el, root);
     });
